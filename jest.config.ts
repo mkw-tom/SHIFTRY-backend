8a4c55ddd66631c4f179fd675bae988e002e103b
Tomu Maekawa <tomu.m1107@icloud.com>
2025-03-02 21:55:28 +0900

    add integration test for user and add action for testing

--- jest.config.ts
+++ jest.config.ts
@@ -6,10 +6,11 @@ module.exports = {
   // },
   preset: "ts-jest",
   testMatch: ["**/tests/**/*.test.ts"], // テストファイルのパターン
-  setupFiles: ["dotenv/config"], // `.env` の環境変数をロード
+  // setupFiles: ["dotenv/config"], // `.env` の環境変数をロード
   clearMocks: true, // 各テストの後にモックをクリア
   coverageDirectory: "coverage", // カバレッジレポートの出力先
   collectCoverageFrom: ["src/**/*.ts"], // カバレッジ収集対象
   moduleFileExtensions: ["ts", "js", "json"],
   moduleDirectories: ["node_modules", "src"],
+  setupFiles: ["<rootDir>/jest.setup.ts"],
 };
