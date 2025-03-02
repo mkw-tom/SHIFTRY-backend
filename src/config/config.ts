686e42b96fc40c4dcaa4488251f5240f7e7e7b6f
Tomu Maekawa <tomu.m1107@icloud.com>
2025-03-03 07:18:24 +0900

    fix ci error

--- src/config/config.ts
+++ src/config/config.ts
@@ -1,7 +1,8 @@
 import dotenv from "dotenv";
 
 // `NODE_ENV` に応じて適用する `.env` ファイルを決定
-const envFile = process.env.NODE_ENV === "test" ? ".env.test" : ".env.production";
+const envFile =
+	process.env.NODE_ENV === "test" ? ".env.test" : ".env.production";
 dotenv.config({ path: envFile });
 
 console.log(`🚀 Using ${envFile} configuration`);
