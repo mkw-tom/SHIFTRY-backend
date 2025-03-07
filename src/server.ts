8e2800d9949f2b37a60c666cc65d85bf577784a2
Tomu Maekawa <tomu.m1107@icloud.com>
2025-03-07 15:30:13 +0900

    refactoer script in package.json and formatted code

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,10 @@
+import dotenv from "dotenv";
 import app from "./app";
+dotenv.config();
 
 const PORT = process.env.PORT || 3000;
 
 app.listen(PORT, () => {
 	console.log(`🚀 Server running on port ${PORT}`);
+	console.log(process.env.LINE_API_URL, process.env.LINE_ACCESS_TOKEN);
 });
