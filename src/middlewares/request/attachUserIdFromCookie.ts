27c7527f7a636ddf628123c368f92cf837725aba
Tomu Maekawa <tomu.m1107@icloud.com>
2025-04-05 16:08:14 +0900

    Added cookie authentication logic

--- src/middlewares/request/attachUserIdFromCookie.ts
+++ src/middlewares/request/attachUserIdFromCookie.ts
@@ -1,8 +1,16 @@
+import { parse } from "cookie";
 import type { NextFunction, Request, Response } from "express";
 import jwt from "jsonwebtoken";
 
-export const verifyJWT = (req: Request, res: Response, next: NextFunction) => {
-	const token = req.headers.authorization?.split(" ")[1];
+export const attachUserIdFromCookie = (
+	req: Request,
+	res: Response,
+	next: NextFunction,
+) => {
+	const cookieHeader = req.headers.cookie || "";
+	const cookies = parse(cookieHeader);
+
+	const token = cookies.token;
 
 	if (!token) {
 		res.status(401).json({ message: "No token provided" });
@@ -13,11 +21,14 @@ export const verifyJWT = (req: Request, res: Response, next: NextFunction) => {
 		const decoded = jwt.verify(token, process.env.JWT_SECRET as string) as {
 			userId: string;
 		};
+		if (!decoded.userId) {
+			res.status(401).json({ message: "Invalid token" });
+			return;
+		}
 
 		req.userId = decoded.userId;
-		next();
+		next(); // ✅ ここで controller 側に渡す！
 	} catch (err) {
 		res.status(401).json({ message: "Invalid token" });
-		return;
 	}
 };
