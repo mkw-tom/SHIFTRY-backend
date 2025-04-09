7238f9440f1e7448589e57e34f8c5bf6fb19d3aa
Tomu Maekawa <tomu.m1107@icloud.com>
2025-04-09 11:31:35 +0900

    Restructured shift-related tables and validations

---
+++ src/types/submittedShift.type.ts
@@ -0,0 +1,4 @@
+export interface submittedShifts {
+	off: [{ date: string; time: string | null }];
+	details: string;
+}
--- src/validations/submittedShift.vaidation.ts
+++ src/validations/submittedShift.vaidation.ts
@@ -1,33 +1,37 @@
 import { ShiftStatus } from "@prisma/client";
 import { z } from "zod";
 
-const timeSlotValidate = z.object({
-	time: z.string(), // より厳密にするなら後述
-	required: z.number().min(1),
-});
+// const timeSlotValidate = z.object({
+// 	time: z.string(), // より厳密にするなら後述
+// 	required: z.number().min(1),
+// });
 
-const ShiftToSubmitValidate = z.object({
-	monday: z.array(timeSlotValidate),
-	tuesday: z.array(timeSlotValidate),
-	wednesday: z.array(timeSlotValidate),
-	thursday: z.array(timeSlotValidate),
-	friday: z.array(timeSlotValidate),
-	saturday: z.array(timeSlotValidate),
-	sunday: z.array(timeSlotValidate),
-});
+// const ShiftToSubmitValidate = z.object({
+// 	monday: z.array(timeSlotValidate),
+// 	tuesday: z.array(timeSlotValidate),
+// 	wednesday: z.array(timeSlotValidate),
+// 	thursday: z.array(timeSlotValidate),
+// 	friday: z.array(timeSlotValidate),
+// 	saturday: z.array(timeSlotValidate),
+// 	sunday: z.array(timeSlotValidate),
+// });
 
-export type ShiftToSubmitType = z.infer<typeof ShiftToSubmitValidate>;
+// export type ShiftToSubmitType = z.infer<typeof ShiftToSubmitValidate>;
+export const shiftsOfSubmittedValidate = z.object({
+	off: z.array(
+		z.object({
+			date: z.string().regex(/^\d{4}-\d{2}-\d{2}$/), // YYYY-MM-DD
+			time: z.string().nullable(),
+		}),
+	),
+	details: z.string(),
+});
+export type ShiftsOfSubmittedType = z.infer<typeof shiftsOfSubmittedValidate>;
 
 export const upsertSubmittedShifttValidate = z.object({
 	shiftRequestId: z.string(),
 	attendCount: z.number().min(1).max(7),
-	// weekStart: z.string().refine((val) => !Number.isNaN(Date.parse(val)), {
-	// 	message: "Invalid date format",
-	// }),
-	// weekEnd: z.string().refine((val) => !Number.isNaN(Date.parse(val)), {
-	// 	message: "Invalid date format",
-	// }),
-	shifts: ShiftToSubmitValidate,
+	shifts: shiftsOfSubmittedValidate,
 	status: z.nativeEnum(ShiftStatus, {
 		errorMap: () => ({ message: "Invalid status" }),
 	}),
